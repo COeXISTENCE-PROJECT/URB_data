b3326d554fdb41f5bc744a43723394803274743d
limexcyan <lime.pss@gmail.com>
2025-05-06 19:01:53 +0200

    KPI for second scenario

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Directory containing subfolders
+RESULTS_DIR="results/scenario2"
+
+# Loop through all subdirectories in the results directory
+for dir in "$RESULTS_DIR"/*/; do
+    # Remove trailing slash and extract subfolder name
+    ID=$(basename "$dir")
+
+    # Execute the Python program with the arguments
+    python benchmarkMetrics.py --results_folder "$RESULTS_DIR" --verbose True --id "$ID"
+done
